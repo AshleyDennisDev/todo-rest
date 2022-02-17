bf2591acf5d7a966a0a197e84c5878a72dfbfd9c
Ashley Dennis <adenn1852@gmail.com>
2022-02-17 13:26:09 -0500

    pushed to cloud

---
+++ src/connectDb.js
@@ -0,0 +1,12 @@
+const {cert, initializeApp, getApps} = require('firebase-admin/app')
+const {getFirestore } = require('firebase-admin/firestore')
+const serviceAccount = require('../credentials.json')
+
+exports.connectDb =() =>{
+    if(!getApps().length){
+initializeApp({
+    credential: cert(serviceAccount)
+    })
+    }
+    return getFirestore();
+}
